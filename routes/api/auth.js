b6f88731285914d748747f39e2c1412d5b369786
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2019-05-09 17:53:33 +0200

    users api routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,93 @@
 const express = require('express');
 const router = express.Router();
+const bctypt = require('bcryptjs');
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const User = require('../../models/User');
 
 // @route GET api/auth
 // @desc Test route
 // @access Public
 
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route POST api/auth
+ * @desc Authenticate user and get token
+ * @access Public
+ */
+
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bctypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Return jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.json({ token });
+        }
+      );
+
+      // res.send('User registered');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const {
-  check,
-  validationResult,
-} = require('express-validator/check');
+const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -15,63 +14,87 @@ const User = require('../../models/User');
  * @access Public
  */
 
-router.post('/', [
-  check('name', 'Name is required').not().isEmpty(),
-  check('email', 'Please include a valid email').isEmail(),
-  check('password', 'Please enter a password with 6 or more characters').isLength({
-    min: 6
-  }),
-], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({
-      errors: errors.array()
-    });
-  }
-
-  const {
-    name,
-    email,
-    password
-  } = req.body;
-
-  try {
-    // See if user exists
-    let user = await User.findOne({ email });
-
-    if (user) {
-      return res.status(400).json({ errors: [ { msg: 'User already exists' } ] });
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
 
-    // Get users gravatar
-    const avatar = gravatar.url(email, {
-      s: '200',
-      r: 'pg',
-      d: 'mm',
-    });
+    const { name, email, password } = req.body;
 
-    user = new User({
-      name,
-      email,
-      avatar,
-      password,
-    });
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
 
-    // Encrypt password
-    const salt = await bcrypt.genSalt(10);
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
 
-    user.password = await bcrypt.hash(password, salt);
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
-    await user.save();
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
 
-    // Return jsonwebtoken
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
 
+      user.password = await bcrypt.hash(password, salt);
 
-    res.send('User registered');
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server error');
+      await user.save();
+
+      // Return jsonwebtoken
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.json({ token });
+        }
+      );
+
+      // res.send('User registered');
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server error');
+    }
   }
-});
+);
 
 module.exports = router;
